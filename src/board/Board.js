1b03bd9fce276908a89f3429c4f541f23141d759
atlassubbed <atlassubbed@gmail.com>
2019-03-25 03:32:38 -0400

    simplies the architecture, shows capabilities of nested diffing

--- src/board/Board.js
+++ src/board/Board.js
@@ -1,22 +1,18 @@
 import { t } from "atlas-relax-jsx-pragmas";
 import { obs } from "atlas-munchlax";
 import Piece, { sq } from "./Piece";
-import { dim, width, pieceWidth, fontSize, tauLevels, isTauSplitting, mousePos } from "../shared";
+import { dim, width, fontSize, isTauSplitting, mousePos, tauLevels } from "../shared";
 /**@jsx t*/
 
-const initPos = e => {
-  isTauSplitting(true);
-}
-const resetPos = e => {
-  mousePos(null)
-  isTauSplitting(false);
-}
-const Board = obs((temp, node) => {
+const initPos = e => isTauSplitting(true);
+const resetPos = e => (mousePos(null), isTauSplitting(false));
+const Board = obs(() => {
+  tauLevels(); // trigger changes when levels change 
   const rows = [];
   for (let i = 0; i < dim(); i++){
     const row = [];
     for (let j = 0; j < dim(); j++)
-      row.push(<Piece size={pieceWidth()} width={width()} levels={tauLevels()}/>)
+      row.push(<Piece/>)
     rows.push(row);
   }
   return (
--- src/board/Piece.js
+++ src/board/Piece.js
@@ -1,43 +1,49 @@
 import { Frame, diff } from "atlas-relax";
+import { obs } from "atlas-munchlax";
 import { t } from "atlas-relax-jsx-pragmas";
+import dot from "atlas-dot";
 import { viewCountClient } from "../network";
-import { viewCount, numDiffs, mousePos } from "../shared";
-import TauProvider from "./TauProvider";
+import { viewCount, numDiffs, mousePos, maxTau, width, tauLevels, pieceWidth } from "../shared";
 /**@jsx t*/
 
+const dist = (v1, v2) => {
+  const delta = [v1[0] - v2[0], v1[1] - v2[1]];
+  return Math.sqrt(dot(delta, delta));
+}
+// returns the color level and the tau value
+const calcTau = (pos, lv) => {
+  const t = maxTau(), l = tauLevels(), m = mousePos(), p = pieceWidth();
+  return m && (lv = dist(m, pos)/p|0) < l ? [lv, lv*t/l] : [l, t];
+}
 const sq = w => `width: ${w}px; height: ${w}px;`
 
-// example of dumb component that
-//   * gets reactive vars passed as props
-//   * gets tau/count updates from sideways providers
+// sometimes doing things the "react" way is not practical and
+// we need to backdoor the VDOM model and write imperative code
+// rendered() is like useLayoutEffect in react; it creates side-effects
+// we can safely read and write to the dom in rendered()
 class Piece extends Frame {
-  constructor(temp, plugins){
-    super(temp, plugins)
-    this.setPos = () => mousePos(this.pos);
+  rendered(){
+    // remember this node's DOM position (mid point in the rect)
+    const dom = this._domNode, lvMax = tauLevels();
+    const r = dom.getBoundingClientRect();
+    const pos = this.pos = [(r.right+r.left)/2, (r.bottom+r.top)/2];
+    // calculate the node's color level and tau
+    const [ lv, tau ] = calcTau(pos);
+    // subscribe to view count using this closure's tau
+    viewCountClient.subscribe(() => this.diff(tau));
+    // unmount (clean up) the last effect
+    diff(null, this.obs);
+    const Effect = obs(() => calcTau(pos)[0] !== lv && this.diff(requestAnimationFrame));
+    // create effect to update this node if tau/color changes
+    this.obs = diff(<Effect/>)
+    // apply color to node
+    dom.style.backgroundColor = lv >= tauLevels() ?
+      `rgb(85,85,255)` : `rgb(45,${Math.floor(255*lv/tauLevels())},45)`;
   }
-  rendered(){ // aka useLayoutEffect
-    const r = this._domNode.getBoundingClientRect();
-    this.pos = [(r.right+r.left)/2, (r.bottom+r.top)/2];
-    this.tauProvider = this.tauProvider || diff(<TauProvider
-      getPos={() => this.pos}
-      onChange={(tau, level) => {
-        if (this.tau = tau, level !== this.level){
-          this.level = level;
-          this.diff(requestAnimationFrame);
-        }
-      }}
-    />)
-  }
-  render({data: { size, levels } }){
+  render(){
     numDiffs(numDiffs()+1)
-    viewCountClient.subscribe(() => this.diff(this.tau));
-    const l = this.level;
-    const c = l == null || l >= levels ?
-      `rgb(85,85,255)` :
-      `rgb(45,${Math.floor(255*l/levels)},45)`
-    const s = `${sq(size)} background-color: ${c};`;
     return (
-      <div class="piece" style={s} onMouseEnter={this.setPos}>
+      <div class="piece" style={sq(pieceWidth())} onMouseEnter={() => mousePos(this.pos)}>
         {viewCount()}
       </div>
     )
